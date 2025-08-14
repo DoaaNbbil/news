d99663d5066554c15a0a32480b6ad8624183a4c7
Doa'a Nabeel <doaa2002nabeel7@gmail.com>
2025-08-14 19:11:55 +0300

    create news priject

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(News());
+}
+
+class News extends StatelessWidget {
+  News({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
